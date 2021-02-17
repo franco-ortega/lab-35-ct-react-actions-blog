4606e9c766393083cbebaa6ffbfeb81d2788944c
Franco Ortega <francopdx@gmail.com>
2021-02-17 15:28:53 -0800

    add logic to BlogDetails

--- src/components/blogs/BlogDetails.jsx
+++ src/components/blogs/BlogDetails.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function BlogDetails() {
+export default function BlogDetails({ title, blog }) {
   return (
     <div>
       Blog Details: blog blog bloggy blog blogger.
+      <p>{title}</p>
+      <p>{blog}</p>
     </div>
   );
 }
+
+BlogDetails.propTypes = {
+  title: PropTypes.string.isRequired,
+  blog: PropTypes.string.isRequired,
+};
