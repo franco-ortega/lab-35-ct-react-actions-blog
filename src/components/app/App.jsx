776b66c1041c035769adf5659d7a60709254815c
Franco Ortega <francopdx@gmail.com>
2021-02-17 14:12:03 -0800

    add initial folders and files

---
+++ src/components/app/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Blog from '../../containers/blogs/Blog';
+
+export default function App() {
+  return (
+    <Blog />
+  );
+}
---
+++ src/containers/blogs/Blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import BlogDetails from '../../components/blogs/BlogDetails';
+
+export default function Blog() {
+  return (
+    <div>
+      Blog: This is the blog!
+      <BlogDetails />
+    </div>
+  );
+}
