a64b7e3a23ad5f85f0ca7739616a8655518e7065
Franco Ortega <francopdx@gmail.com>
2021-02-17 14:25:17 -0800

    add BlogForm

--- src/containers/blogs/Blog.jsx
+++ src/containers/blogs/Blog.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import BlogDetails from '../../components/blogs/BlogDetails';
+import BlogForm from './BlogForm';
 
 export default function Blog() {
   return (
     <div>
       Blog: This is the blog!
       <BlogDetails />
+      <BlogForm />
     </div>
   );
 }
---
+++ src/containers/blogs/BlogForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function BlogForm() {
+  return (
+    <div>
+      <p>Blog Form</p>
+      <form action="">
+        Title: <input type="text"/>
+        Blog: <textarea name="" id="" cols="30" rows="10"></textarea>
+        <button>Blog It!</button>
+      </form>
+    </div>
+  );
+}
