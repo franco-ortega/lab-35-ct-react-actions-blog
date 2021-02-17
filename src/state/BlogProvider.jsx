f2d7bd8cc6252cc852719c2997bd10dd8591b24a
Franco Ortega <francopdx@gmail.com>
2021-02-17 14:59:51 -0800

    add BlogProvider

---
+++ src/state/BlogProvider.jsx
@@ -0,0 +1,24 @@
+import React, { createContext, useContext, useReducer } from 'react';
+import reducer, { initialState } from '../reducers/postReducer';
+
+const BlogContext = createContext(null);
+
+export const BlogProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <BlogContext.Provider value={{ state, dispatch }}>
+      {children}
+    </BlogContext.Provider>
+  );
+};
+
+export const useBlogState = () => {
+  const { state } = useContext(BlogContext);
+  return state;
+};
+
+export const useDispatch = () => {
+  const { dispatch } = useContext(BlogContext);
+  return dispatch;
+};
