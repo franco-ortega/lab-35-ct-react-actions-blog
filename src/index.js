407505e12207c4d66a5dc2b6b86c100f83f58a20
Franco Ortega <francopdx@gmail.com>
2021-02-18 13:26:12 -0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app/App';
+import { Provider } from 'react-redux';
 
 render(
-  <App />,
+  <Provider>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
