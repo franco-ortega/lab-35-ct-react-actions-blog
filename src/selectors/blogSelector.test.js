7d3a521b9841b70187d0492dc9201f1006e6fcad
Franco Ortega <francopdx@gmail.com>
2021-02-17 22:57:02 -0800

    add selector test

---
+++ src/selectors/blogSelector.test.js
@@ -0,0 +1,29 @@
+import { getBlogs } from './blogSelector';
+
+describe('blog selectors', () => {
+  it('select the list of blogs from state', () => {
+    const state = {
+      blogs: [{
+        title: 'My first post',
+        blog: 'All of my words!!!'
+      },
+      {
+        title: 'My second post',
+        blog: 'Two.'
+      }]
+    };
+
+    const blogs = getBlogs(state);
+
+    expect(blogs).toEqual(
+      [{
+        title: 'My first post',
+        blog: 'All of my words!!!'
+      },
+      {
+        title: 'My second post',
+        blog: 'Two.'
+      }]
+    );
+  });
+});
