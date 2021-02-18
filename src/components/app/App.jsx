4eca39e4119322c6ce8ae1404c1e7520a0f899c5
Franco Ortega <francopdx@gmail.com>
2021-02-18 14:03:37 -0800

    remove BlogProvider

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import BlogForm from '../../containers/blogs/BlogForm';
 import BlogList from '../../containers/blogs/BlogList';
-import { BlogProvider } from '../../state/BlogProvider';
 
 export default function App() {
   return (
-    <BlogProvider>
+    <>
       <h1>Welcome to the Blog</h1>
       <BlogForm />
       <BlogList />
-    </BlogProvider>
+    </>
   );
 }
