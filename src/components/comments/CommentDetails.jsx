6d2039c27bf688de02cf0f99b3e0a9fd372f7a37
Franco Ortega <francopdx@gmail.com>
2021-02-19 14:22:19 -0800

    add delete button CommentDetails

--- src/components/comments/CommentDetails.jsx
+++ src/components/comments/CommentDetails.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './CommentDetails.css';
+import { useDispatch } from 'react-redux';
+import { deleteComment } from '../../actions/commentActions';
 
 export default function CommentDetails({ blogIndex, commentIndex, comment }) {
+  const dispatch = useDispatch();
+
+  const onDeleteCommentClick = () => {
+    console.log('Delete Comment button clicked');
+    dispatch(deleteComment(commentIndex));
+  };
+
   return (
     <div className={styles.CommentDetails}>
       Comment:
       <p>Blog #: {blogIndex}</p>
       <p>Comment #: {commentIndex}</p>
       <p>Comment: {comment}</p>
+      <button onClick={onDeleteCommentClick}>Delete Comment</button>
     </div>
   );
 }
